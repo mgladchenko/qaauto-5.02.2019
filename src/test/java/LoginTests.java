cf7d5d60677dd63e8a602d64d3ffb86aaf693d27
Mykola Gladchenko <mykola.gladchenko@gmail.com>
2019-02-15 12:47:18 +0200

    Lesson 4

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,14 +1,47 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests {
 
+    WebDriver driver;
+
+    @BeforeMethod
+    public void beforeMethod() {
+        System.setProperty("webdriver.chrome.driver", "/Users/mykola/projects/qaauto-5.02.2019/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("https://www.linkedin.com/");
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        driver.quit();
+    }
+
     @Test
     public void successfulLoginTest() {
-        System.setProperty("webdriver.chrome.driver", "/Users/mykola/projects/qaauto-5.02.2019/chromedriver");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.linkedin.com/");
+        WebElement userEmailField = driver.findElement(By.xpath("//input[@id='login-email']"));
+        WebElement userPasswordField = driver.findElement(By.xpath("//input[@id='login-password']"));
+        WebElement signInButton = driver.findElement(By.xpath("//input[@id='login-submit']"));
+
+        userEmailField.sendKeys("linkedin.tst.yanina@gmail.com");
+        userPasswordField.sendKeys("Test123!");
+        signInButton.click();
+
+        WebElement profileMenuItem = driver.findElement(By.xpath("//li[@id='profile-nav-item']"));
+
+        Assert.assertTrue(profileMenuItem.isDisplayed(), "profileMenuItem is not displayed on Home page.");
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/feed/",
+                "Home page URL is incorrect.");
+    }
+
+    @Test
+    public void negativeLoginTest() {
 
     }
 }
