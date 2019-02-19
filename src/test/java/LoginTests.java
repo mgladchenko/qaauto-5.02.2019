114e12d3925d932534e32f8514ce2f2e95c00483
Mykola Gladchenko <mykola.gladchenko@gmail.com>
2019-02-19 13:15:13 +0200

    Lesson 5

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -25,23 +25,34 @@ public class LoginTests {
 
     @Test
     public void successfulLoginTest() {
-        WebElement userEmailField = driver.findElement(By.xpath("//input[@id='login-email']"));
-        WebElement userPasswordField = driver.findElement(By.xpath("//input[@id='login-password']"));
-        WebElement signInButton = driver.findElement(By.xpath("//input[@id='login-submit']"));
-
-        userEmailField.sendKeys("linkedin.tst.yanina@gmail.com");
-        userPasswordField.sendKeys("Test123!");
-        signInButton.click();
+        LandingPage landingPage = new LandingPage(driver);
+        landingPage.login("linkedin.tst.yanina@gmail.com", "Test123!");
 
         WebElement profileMenuItem = driver.findElement(By.xpath("//li[@id='profile-nav-item']"));
 
         Assert.assertTrue(profileMenuItem.isDisplayed(), "profileMenuItem is not displayed on Home page.");
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/feed/",
                 "Home page URL is incorrect.");
+        //logout
     }
 
     @Test
     public void negativeLoginTest() {
+        WebElement userEmailField = driver.findElement(By.xpath("//input[@id='login-email']"));
+        WebElement userPasswordField = driver.findElement(By.xpath("//input[@id='login-password']"));
+        WebElement signInButton = driver.findElement(By.xpath("//input[@id='login-submit']"));
 
+        userEmailField.sendKeys("linkedin.tst.yanina@gmail.com");
+        userPasswordField.sendKeys("12345");
+        signInButton.click();
+
+        WebElement passwordErrorMessageBlock = driver.findElement(By.xpath("//div[@id='error-for-password']"));
+
+        Assert.assertTrue(passwordErrorMessageBlock.isDisplayed(),
+                "passwordErrorMessageBlock is not displayed on Home page.");
+
+        Assert.assertEquals(passwordErrorMessageBlock.getText(),
+                "Hmm, that's not the right password. Please try again or request a new one.",
+                "Wrong validation message text for 'password' field.");
     }
 }
