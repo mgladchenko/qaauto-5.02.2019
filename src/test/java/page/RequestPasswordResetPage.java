e238b459f3dac528850144d9a0d1db271578b983
Mykola Gladchenko <mykola.gladchenko@gmail.com>
2019-03-27 11:22:13 +0200

    Lesson 12 (partial)

--- src/test/java/page/RequestPasswordResetPage.java
+++ src/test/java/page/RequestPasswordResetPage.java
@@ -1,9 +1,11 @@
 package page;
 
+import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import util.GMailService;
 
 public class RequestPasswordResetPage {
     WebDriver driver;
@@ -28,6 +30,20 @@ public class RequestPasswordResetPage {
     public RequestPasswordResetSubmitPage findAccount(String userEmail) {
         userEmailField.sendKeys(userEmail);
 
+        String messageSubject = "here's the link to reset your password";
+        String messageTo = userEmail;
+        String messageFrom = "security-noreply@linkedin.com";
+
+        GMailService gMailService = new GMailService();
+        gMailService.connect();
+        findAccountButton.click();
+
+        String message = gMailService.waitMessage(messageSubject, messageTo, messageFrom, 180);
+        System.out.println("Content: " + message);
+
+        String resetPasswordUrl = StringUtils.substringBetween(message, "href=\"", "\" style=\"cursor:pointer;color:#008CC9;-webkit-text-size-adjust:100%;display:inline-block;text-decoration:none;-ms-text-size-adjust:100%;\">Reset my password\n");
+
+        resetPasswordUrl.replace("amp;", "");
         return new RequestPasswordResetSubmitPage(driver);
     }
 }
